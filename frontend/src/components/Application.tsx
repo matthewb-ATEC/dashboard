bd4071a1175deededb17652554223643f16d4446
matthewb-ATEC <matthewb@advancetecllc.com>
2024-11-22 10:55:02 -0500

    disabled apps more clear

--- frontend/src/components/Application.tsx
+++ frontend/src/components/Application.tsx
@@ -34,11 +34,11 @@ const Application = ({
     <div className="flex flex-grow rounded-md bg-white shadow-md flex items-center text-center justify-center">
       <div className="flex flex-col space-y-4 p-8">
         {/* Icons provided by google fonts icons */}
-        <i className="material-icons text-gray-500 text-5xl">{iconName}</i>
+        <i className="material-icons text-gray-400 text-5xl">{iconName}</i>
 
         <div className="flex flex-col space-y-2">
-          <div className="font-semibold text-2xl">{title}</div>
-          <div className="text-gray-500 text-wrap">{description}</div>
+          <div className="font-semibold text-gray-400 text-2xl">{title}</div>
+          <div className="text-gray-300 text-wrap">{description}</div>
         </div>
       </div>
     </div>
